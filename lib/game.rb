edc5f4b33317838572991a61996fd2767b2b997c
ERIC KELLY <eakelly220@gmail.com>
2018-05-14 16:24:04 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -14,10 +14,10 @@ class Game
 
 
 
-  def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
+  def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"))
     @player_1 = player_1
     @player_2 = player_2
-    @board = board
+    @board = Board.new
   end
 
   def current_player
